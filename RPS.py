e5fb8ffa7b720bf3ba47ebdb24e6ae140cddeb41
WillHeadlee <william.headlee182@gmail.com>
2026-02-02 16:29:37 +0000

    Lab 3 Complete

--- RPS.py
+++ RPS.py
@@ -1,16 +1,40 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: William Headlee
+#Date: 2/2/26
+#Assignment: Lab 3
 import random
 
 def main():
   wins = 0
   ties = 0
   losses = 0
+  playAgain = True
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
-
+  while playAgain:
+    print("")
+    print("Let's play Rock, Paper, Scissors!")
+    choices = ["R", "P", "S"]
+    RPS = input("Rock, Paper, or Scissors? (Please type only R, P, or S) ")
+    CRPS = random.choice(choices)
+    if (RPS != "R") and (RPS != "P") and (RPS != "S"):
+      print("That's not a valid choice!")
+    elif (RPS == "R" and CRPS == "S") or (RPS == "S" and CRPS == "P") or (RPS == "P" and CRPS == "R"):
+      wins += 1
+      print("Computer played " + CRPS)
+      print("You Win!")
+    elif (CRPS == "R" and RPS == "S") or (CRPS == "S" and RPS == "P") or (CRPS == "P" and RPS == "R"):
+      losses += 1
+      print("Computer played " + CRPS)
+      print("You lost :(")
+    else:
+      ties += 1
+      print("Computer played " + CRPS)
+      print("It's a tie!")
+    playAgain = input("Would you like to play again? (Y or N) ")
+    if playAgain == "N":
+      playAgain = False
+    
   #Randomly choose the computer between 'R', 'P', or 'S'
   #Prompt the user for their RPS selection
   #Determine winner and state what happened to the user
--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,19 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: William Headlee
+#Date: 2/2/26
+#Assignment: Lab 3
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
+  print("Fehrenheit to Celsius Converter")
+  
+  tempF = int(input("Provide a temperature in F: "))
 
-  tempC = tempF / 2
+  tempC = round((tempF - 32)/1.8, 1)
 
-  print(tempF, "is ", tempC, "degrees celsius.")
+  print(tempF, "is", tempC, "degrees celsius.")
 if __name__ == '__main__':
   main()
