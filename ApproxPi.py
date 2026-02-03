1af818195e8c7354df516bb8db25661c2152410c
WillHeadlee <william.headlee182@gmail.com>
2026-02-03 03:38:35 +0000

    Extra Credit Complete

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,7 +1,7 @@
 #ApproxPi.py
-#Name:
-#Date:
-#Assignment:
+#Name: William Headlee
+#Date: 2/2/26
+#Assignment: Lab 3
 import math
 import time
 
@@ -11,11 +11,29 @@ import time
 def main():
   realPi = math.pi
 
-  #ask user for decimal percision (up to 10)
+  #ask user for decimal precision (up to 10)
+
+  precision = int(input("How precise would you like to be (up to 10, anything over 4 takes a bit): "))
+  roundedPi = round(realPi, precision)
+  attempts = 0
+  calculatedPi = 0
+  series = [1]
+  n = 1
+  alternation = -1
+  print("Calculating... ")
 
   start = time.time()
   #calculate pi using the approximation technique
-  #Loop until the level of percision is reached
+  #Loop until the level of precision is reached
+
+  while roundedPi != round((calculatedPi*4), precision):
+    calculatedPi = 0
+    for i in series:
+      calculatedPi += i
+    attempts += 1
+    n += 2
+    series.append((1/n)*alternation)
+    alternation *= -1
 
   end = time.time()
 
